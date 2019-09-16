5e8f1cf8a11ab3d98c4d7bbe84d26b56d4210fcd
Kang-min Liu <gugod@gugod.org>
2019-09-17 08:42:03 +0900

    Remove the dependency Rubyish.

--- lib/Parse/USDASR.pm
+++ lib/Parse/USDASR.pm
@@ -1,54 +1,61 @@
+package Parse::USDASR;
+use 5.008;
 use strict;
 use warnings;
-use Rubyish;
 
-class Parse::USDASR {
-    use 5.008;
-    our $VERSION = '0.02';
+our $VERSION = '0.03';
 
-    attr_accessor "io";
+my %field_name = (
+    FOOD_DES  => [qw(NDB_No FdGrp_Cd Long_Desc Shrt_Desc ComName ManufacName Survey Ref_desc Refuse SciName N_Factor Pro_Factor Fat_Factor CHO_Factor)],
+    FD_GROUP  => [qw(FdGrp_Cd FdGrp_Desc)],
+    NUT_DATA  => [qw(NDB_No Nutr_No Nutr_Val Num_Data_Pts Std_Error Src_Cd Deriv_Cd Ref_NDB_No Add_Nutr_Mark Num_Studies Min Max DF Low_EB Up_EB Stat_cmt CC)],
+    NUTR_DEF  => [qw(Nutr_No Units Tagname NutrDesc Num_Desc SR_Order)],
+    SRC_CD    => [qw(Src_Cd SrcCd_Desc)],
+    DERIV_CD  => [qw(Deriv_Cd Deriv_Desc)],
+    WEIGHT    => [qw(NDB_No Seq Amount Msre_Desc Gm_Wgt Num_Data_Pts Std_Dev)],
+    FOOTNOTE  => [qw(NDB_No Footnt_No Footnt_Typ Nutr_No Footnt_Txt)],
+    DATSRCLN  => [qw(NDB_No Nutr_No DataSrc_ID)],
+    DATA_SRC  => [qw(DataSrc_ID Authors Title Year Journal Vol_City Issue_State Start_Page End_Page)],
+    ABBREV    => [qw(NDB_No Shrt_Desc Water Energ_Kcal Protein Lipit_Tot Ash Carbonhydrt Fiber_TD Sugar_Tot Calcium Iron Magnesium Phosphorus Potassium Sodium Zinc Copper Manganese Selenium Vit_C Thiamin Riboflavin Niacin Panto_acid Vit_B6 Folate_Tot Folic_acid Food_Folate Folate_DFE Choline_total Vit_B12 Vit_A_IU Vit_A_RAE Retinol Alpha_Carot Beta_Carot Beta_Crypt Lycopene Lut_and_Zea Vit_E Vit_K FA_Sat FA_Mono FA_Poly Cholestrl GmWt_1 GmWt_Desc1 GmWt_2 GmWt_Desc2 Refuse_Pct)]
+);
 
-    my %field_name = (
-        FOOD_DES  => [qw(NDB_No FdGrp_Cd Long_Desc Shrt_Desc ComName ManufacName Survey Ref_desc Refuse SciName N_Factor Pro_Factor Fat_Factor CHO_Factor)],
-        FD_GROUP  => [qw(FdGrp_Cd FdGrp_Desc)],
-        NUT_DATA  => [qw(NDB_No Nutr_No Nutr_Val Num_Data_Pts Std_Error Src_Cd Deriv_Cd Ref_NDB_No Add_Nutr_Mark Num_Studies Min Max DF Low_EB Up_EB Stat_cmt CC)],
-        NUTR_DEF  => [qw(Nutr_No Units Tagname NutrDesc Num_Desc SR_Order)],
-        SRC_CD    => [qw(Src_Cd SrcCd_Desc)],
-        DERIV_CD  => [qw(Deriv_Cd Deriv_Desc)],
-        WEIGHT    => [qw(NDB_No Seq Amount Msre_Desc Gm_Wgt Num_Data_Pts Std_Dev)],
-        FOOTNOTE  => [qw(NDB_No Footnt_No Footnt_Typ Nutr_No Footnt_Txt)],
-        DATSRCLN  => [qw(NDB_No Nutr_No DataSrc_ID)],
-        DATA_SRC  => [qw(DataSrc_ID Authors Title Year Journal Vol_City Issue_State Start_Page End_Page)],
-        ABBREV    => [qw(NDB_No Shrt_Desc Water Energ_Kcal Protein Lipit_Tot Ash Carbonhydrt Fiber_TD Sugar_Tot Calcium Iron Magnesium Phosphorus Potassium Sodium Zinc Copper Manganese Selenium Vit_C Thiamin Riboflavin Niacin Panto_acid Vit_B6 Folate_Tot Folic_acid Food_Folate Folate_DFE Choline_total Vit_B12 Vit_A_IU Vit_A_RAE Retinol Alpha_Carot Beta_Carot Beta_Crypt Lycopene Lut_and_Zea Vit_E Vit_K FA_Sat FA_Mono FA_Poly Cholestrl GmWt_1 GmWt_Desc1 GmWt_2 GmWt_Desc2 Refuse_Pct)]
-    );
+sub new {
+    my ($class) = @_;
+    return bless {}, $class;
+}
 
-    def field_names_for {
-        my ($filename) = @_;
-        $filename =~ s/.txt$//;
-        $filename = uc($filename);
-        @{$field_name{$filename}};
-    }
+sub io {
+    my ($self, $v) = @_;
+    $self->{_io} = $v if $v;
+    return $self->{_io};
+}
 
-    def parse_line {
-        my ($line) = @_;
-        map {
-            $a = $_;
-            $a =~ s/^\~//;
-            $a =~ s/\~$//;
-            $a;
-        } split /\^/, $line;
-    };
+sub field_names_for {
+    my (undef, $filename) = @_;
+    $filename =~ s/.txt$//;
+    $filename = uc($filename);
+    @{$field_name{$filename}};
+}
 
-    def each_line {
-        my $io = $self->io;
-        my ($sub) = @_;
-        while (my $line = <$io>) {
-            chomp($line);
-            my $ret = $sub->( $self->parse_line($line) );
-            last if defined($ret) && !$ret;
-        }
-    }
+sub parse_line {
+    my ($self, $line) = @_;
+    map {
+        $a = $_;
+        $a =~ s/^\~//;
+        $a =~ s/\~$//;
+        $a;
+    } split /\^/, $line;
 };
+
+sub each_line {
+    my ($self, $sub) = @_;
+    my $io = $self->io;
+    while (my $line = <$io>) {
+        chomp($line);
+        my $ret = $sub->( $self->parse_line($line) );
+        last if defined($ret) && !$ret;
+    }
+}
 
 1;
 __END__
@@ -129,7 +136,7 @@ Kang-min Liu E<lt>gugod@gugod.orgE<gt>
 
 =head1 SEE ALSO
 
-L<http://www.usda.gov/>, L<http://www.nal.usda.gov/fnic/foodcomp/Data/SR21/dnload/>
+L<https://www.usda.gov/>,L<http://www.nal.usda.gov/fnic/foodcomp/Data/SR21/dnload/>
 
 =head1 LICENSE
 
